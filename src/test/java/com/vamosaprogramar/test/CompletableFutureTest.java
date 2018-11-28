becca720560cffd70bde36efdddeb95bf16ff0ea
Naren David de Avila Julio - Ceiba Software <naren.deavila@ceiba.com.co>
2018-11-28 12:01:21 -0500

    Chaining futures

--- src/test/java/com/vamosaprogramar/test/CompletableFutureTest.java
+++ src/test/java/com/vamosaprogramar/test/CompletableFutureTest.java
@@ -2,6 +2,7 @@ package com.vamosaprogramar.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.sql.Time;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -43,5 +44,60 @@ public class CompletableFutureTest {
 		assertEquals(100, actual, 0.0);
 	}
 	
+	@Test
+	public void completableFutureWithThenAccept() throws InterruptedException, ExecutionException {
+		
+		CompletableFuture<Integer> completableFuture =
+				CompletableFuture.supplyAsync(() -> 23);
+		
+		CompletableFuture<Void> completableFuture2 =
+				completableFuture.thenAccept(x -> System.out.println("I'm "+x+" years old"));
+		//Blocking
+		completableFuture2.get();
+		
+	}
+	
+	@Test
+	public void completableFutureWithThenRun() {
+		
+		CompletableFuture<String> completableFuture = 
+				CompletableFuture.supplyAsync(() -> "Good morning mom");
+		CompletableFuture<Void> completableFuture2 =
+				completableFuture.thenRun(() -> System.out.println("Morning son! How was ur night"));
+	}
+	
+	@Test
+	public void completableFutureWithThenCompose() throws InterruptedException, ExecutionException {
+		CompletableFuture<String> completableFuture = 
+				CompletableFuture.supplyAsync(() -> "Morning Naren!")
+				.thenCompose(x -> CompletableFuture.supplyAsync(() -> x+" How are you?"));
+		
+		//Blocking
+		String actual = completableFuture.get();
+		
+		assertEquals("Morning Naren! How are you?", actual);
+	}
+	
+	@Test
+	public void completableFutureWithThenCombine() throws InterruptedException, ExecutionException {
+		
+		CompletableFuture<Double> completableFuture =
+				CompletableFuture.supplyAsync(() -> 23.0)
+				.thenCombine(CompletableFuture.supplyAsync(() -> 10.0), (x, y) -> x * y);
+		//Blocking
+		double actual = completableFuture.get();
+		
+		assertEquals(230.0, actual, 0.0);
+	}
+	
+	
+	@Test
+	public void completableFutureWithThenAcceptBoth() {
+		
+		CompletableFuture<Void> completableFuture = 
+				CompletableFuture.supplyAsync(() -> 10)
+				.thenAcceptBoth(CompletableFuture.supplyAsync(() -> 13), (x, y) -> System.out.println("My age is "+(x+y)));
+		
+	}
 	
 }
